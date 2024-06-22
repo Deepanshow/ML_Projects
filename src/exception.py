dea28229fa30386a444d0caa10df4620db814c4b
Deepanshu Gurjar <deepanshu.gurjar@niser.ac.in>
2024-06-22 20:54:59 +0530

    changes

--- src/exception.py
+++ src/exception.py
@@ -2,6 +2,7 @@
 
 
 import sys 
+from src.logger import logging
 
 def error_message_details(error,error_detail:sys ):
     _,_,exc_tb = error_detail.exc_info() # Execution info -- last information is important where is error ( line number etc)
@@ -18,5 +19,3 @@ class CustomException(Exception):
     def __str__(self):
         return self.error_message
     
-
-
